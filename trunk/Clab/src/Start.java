0a6fd7e3400a45db00d414e1e9a66e415341e7a6
ra64kon <ra64kon@bcffeeb5-8122-3b48-ef5a-4a7c17ab8109>
2015-02-19 21:25:08 +0000

    test

--- trunk/Clab/src/Start.java
+++ trunk/Clab/src/Start.java
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 public class Start
 {
-	private static final double version = 0.07;
+	private static final double version = 0.07; //test
 	
 	public static void main(String[] args) throws IOException
     {
